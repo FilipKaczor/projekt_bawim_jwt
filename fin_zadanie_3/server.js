d24adb95d87a91976ca8661fa3f8c78620104298
kaczor pog <63914445+FilipKaczor@users.noreply.github.com>
2024-11-21 19:16:34 +0100

    Add files via upload

---
+++ fin_zadanie_3/server.js
@@ -0,0 +1,28 @@
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const path = require('path')
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`)
+    next()
+}) //logger, pokazuje logi w terminalu
+
+app.use(express.urlencoded({ extended: false })) //handle urlencoded form data
+app.use(express.json()) //obsługa json
+app.use(express.static(path.join(__dirname, 'public'))) //obsługa plików statycznych np. css z folderu public
+app.use(cookieParser())
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'src', 'index.html'))
+// })
+
+app.use('/', require('./routes/main_route.js'))
+app.use('/register', require('./routes/registerRoute.js'))
+app.use('/auth', require('./routes/authRoute.js'))
+app.use('/refresh', require('./routes/refresh.js'))
+
+
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`))
